b0ba1b25e23e2543a522f66ccc3b04a8113452dd
gopalingole20 <104223669+gopalingole20@users.noreply.github.com>
2022-04-25 12:45:34 +0530

    Update Demo.java

--- Github2Project/src/github/com/Demo.java
+++ Github2Project/src/github/com/Demo.java
@@ -9,9 +9,9 @@ public class Demo {
 	{
 		  System.out.println("m2 of Demo");
 	}
-	public void m2()
+	public void m3()
 	{
-		  System.out.println("m2 of Demo");
+		  System.out.println("m3 of Demo");
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
