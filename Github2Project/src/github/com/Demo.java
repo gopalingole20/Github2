f8e0af6bc92bff10978d7dd20928e79a277bf274
gopalingole20 <104223669+gopalingole20@users.noreply.github.com>
2022-04-25 12:11:48 +0530

    Update Demo.java

--- Github2Project/src/github/com/Demo.java
+++ Github2Project/src/github/com/Demo.java
@@ -1,7 +1,10 @@
 package github.com;
 
 public class Demo {
-
+        public void m1()
+	{
+		  System.out.println("m1 of Demo");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("second github project");
